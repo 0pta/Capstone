cab29ba0e6bc5db3eb77e2f0c32bc7ce8fcb55c5
Aaron Eugene Kester <aaron.opta@yahoo.com>
2017-07-18 23:04:41 -0700

    correctly injected ngClipboard

--- public/js/app.module.js
+++ public/js/app.module.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict'
 
-  angular.module('app', ['ui.router'])
+  angular.module('app', ['ui.router', 'ngClipboard'])
   .constant('API_BASE_URL', 'http://localhost:3000')
 
 }())
